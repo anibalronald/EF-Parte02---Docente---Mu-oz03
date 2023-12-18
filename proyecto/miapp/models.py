16fca263273370501154db7e6764afa70b8fb3be
Alberto Mondragon <ismael20_mondragon@hotmail.com>
2023-12-18 10:17:27 -0500

    EF Parte02(1) - Docente - Mondragon 02

--- proyecto/miapp/models.py
+++ proyecto/miapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Docente(models.Model):
+    codigo=models.PositiveSmallIntegerField()
+    nombre=models.CharField(max_length=1000)
+    apellido_paterno=models.CharField(max_length=1000)
+    apellido_materno=models.CharField(max_length=1000)
+    dni=models.PositiveSmallIntegerField()
+    fecha_nac=models.DateField()
+    fecha_reg=models.DateTimeField(auto_now_add=True)
--- proyecto/miapp/views.py
+++ proyecto/miapp/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect,HttpResponse
+from miapp.models import Docente
+from django.contrib import messages
 def index (request):
     return render(request, 'index.html')
 def crearcurso(request):
     return render(request,'crearcurso.html')
 def creardocente(request):
     return render(request,'creardocente.html')
+def guardardocente(request):
+    if request.method == 'POST':
+        codigo=request.POST['codigo']
+        nombre=request.POST['nombre']
+        apellido_paterno=request.POST['apellido_paterno']
+        apellido_materno=request.POST['apellido_materno']
+        dni=request.POST['dni']
+        fecha_nac=request.POST['fecha_nac']
+        estado=request.POST['estado']
+    else:
+        return HttpResponse("<h2>No se ha podido resgitrar el docente</h2>")
+    docente=Docente(
+        codigo=codigo,
+        nombre=nombre,
+        apellido_paterno=apellido_paterno,
+        apellido_materno=apellido_materno,
+        dni=dni,
+        fecha_nac=fecha_nac,
+        estado=estado
+    )
+    docente.save()
+    messages.success(f"<h2>No se ha podido resgitrar el docente: {docente.nombre}</h2>")
+    return redirect('/listardocente')
+def eliminar(request,id):
+    docente=Docente.objects.get(pk=id)
+    docente.delete()
+    return redirect('/listardocente')
 def inicio(request):
     return render(request,'inicio.html')
 def integrantes(request):
@@ -13,4 +41,8 @@ def integrantes(request):
 def listarcurso(request):
     return render(request,'listarcurso.html')
 def listardocente(request):
-    return render(request,'listardocente.html')
+    docente=Docente.objects.all()
+    return render(request,'listardocente.html',{
+        'titulo': 'LISTA DE DOCENTES',
+        'docentes':docente
+    })
